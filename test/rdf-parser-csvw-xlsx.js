6009b4bf0cd4b74f4fdda687df40739097b55a7e
bergi <bergi@axolotlfarm.org>
2020-01-13 15:55:25 +0100

    upgraded dependencies, fixed code style

--- test/rdf-parser-csvw-xlsx.js
+++ test/rdf-parser-csvw-xlsx.js
@@ -1,19 +1,20 @@
-/* global describe, it */
-
 const assert = require('assert')
 const fs = require('fs')
 const path = require('path')
+const { describe, it } = require('mocha')
 const rdf = require('rdf-ext')
-const JsonLdParser = require('rdf-parser-jsonld')
+const JsonLdParser = require('@rdfjs/parser-jsonld')
 const XlsxParser = require('..')
 
 function datasetFromJsonLdFs (filename) {
-  return rdf.dataset().import(JsonLdParser.import(fs.createReadStream(filename), {factory: rdf}))
+  const parser = new JsonLdParser()
+
+  return rdf.dataset().import(parser.import(fs.createReadStream(filename), { factory: rdf }))
 }
 
 describe('rdf-parser-csvw-xlsx', () => {
   it('should be a constructor', () => {
-    assert.equal(typeof XlsxParser, 'function')
+    assert.strictEqual(typeof XlsxParser, 'function')
   })
 
   it('should parse the XLSX file using the given metadata', () => {
@@ -25,7 +26,7 @@ describe('rdf-parser-csvw-xlsx', () => {
       const expected = results[1]
 
       const input = fs.createReadStream(path.join(__dirname, 'support/example.xlsx'))
-      const parser = new XlsxParser({factory: rdf})
+      const parser = new XlsxParser({ factory: rdf })
       const stream = parser.import(input, {
         baseIRI: 'http://example.org/base',
         metadata: metadata,
@@ -33,7 +34,7 @@ describe('rdf-parser-csvw-xlsx', () => {
       })
 
       return rdf.dataset().import(stream).then((dataset) => {
-        assert.equal(dataset.toCanonical(), expected.toCanonical())
+        assert.strictEqual(dataset.toCanonical(), expected.toCanonical())
       })
     })
   })
